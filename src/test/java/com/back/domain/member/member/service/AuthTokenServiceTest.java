a05d5f19f7209080fd4427eae88eac5c6586a8ae
sik2 <bickwang123@gmail.com>
2025-09-18 11:48:25 +0900

    62

--- src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,11 +1,18 @@
 package com.back.domain.member.member.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -20,5 +27,33 @@ public class AuthTokenServiceTest {
     @DisplayName("authTokenService가 존재한다.")
     void t1() {
         assertThat(authTokenService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jjwt 최신 방식으로 jwt 생성, {name= \"Paul\", age=23}")
+    void t2 () {
+        // 서명키 (SecretKey)
+        String jwtSecretKey = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+        byte[] keyBytes = jwtSecretKey.getBytes(StandardCharsets.UTF_8);
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        // 클레임 - 사용자 정보
+        Map<String, Object> claims = Map.of("name", "Paul", "age", "23");
+
+        // 생성시간, 만료시간 설정
+        long expireMillis = 1000L * 60 * 60 * 24 * 365; // 토큰 만료시간을 1년
+        Date issuedAt = new Date(); // 발행시간 설정
+        Date expiration = new Date(issuedAt.getTime() + expireMillis); //발행 시간으로 부터 만료시간 설정
+
+        String jwt = Jwts.builder()
+                .claims(claims) // 사용자 정보
+                .issuedAt(issuedAt) // 생성날짜
+                .expiration(expiration) // 만료날짜
+                .signWith(secretKey) // 키 서명
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt : " + jwt);
     }
 }
