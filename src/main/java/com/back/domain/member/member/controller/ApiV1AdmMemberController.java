7ca07e980c45a101d2eaac487d61cad2ca4dc1bf
sik2 <bickwang123@gmail.com>
2025-09-22 10:35:59 +0900

    81-2

--- src/main/java/com/back/domain/member/member/controller/ApiV1AdmMemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1AdmMemberController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,12 @@ public class ApiV1AdmMemberController {
                 .map(MemberWithUsernameDto::new)
                 .toList();
     }
+
+    @GetMapping("/{id}")
+    public MemberWithUsernameDto getItem(@PathVariable Long id) {
+        Member member = memberService.findById(id).get();
+
+        return new MemberWithUsernameDto(member);
+    }
+
 }
