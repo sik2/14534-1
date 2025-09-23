21bee019475131fcc8bb822b91771119dd6d9c8d
sik2 <bickwang123@gmail.com>
2025-09-23 11:35:08 +0900

    99

--- src/main/java/com/back/global/Rq/Rq.java
+++ src/main/java/com/back/global/Rq/Rq.java
@@ -1,90 +1,35 @@
 package com.back.global.Rq;
 
 import com.back.domain.member.member.entity.Member;
-import com.back.domain.member.member.service.MemberService;
-import com.back.domain.post.post.service.PostService;
-import com.back.global.exception.ServiceException;
+import com.back.global.security.SecurityUser;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class Rq {
-    private final PostService postService;
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
-    private final MemberService memberService;
 
     public Member getActor() {
-        String headerAuthorization = getHeader("Authorization", "");
-
-        String apiKey;
-        String accessToken;
-
-        // headerAuthorization이 존재한다면
-        if (!headerAuthorization.isBlank()) {
-            if (!headerAuthorization.startsWith("Bearer ")) {
-                throw new ServiceException("401-2", "인증 정보가 올바르지 않습니다.");
-            }
-
-            // ["Bearer", apiKey, accessToken]
-            String[] headerAuthorizations =  headerAuthorization.split(" ", 3);
-
-            apiKey = headerAuthorizations[1];
-            accessToken = headerAuthorizations.length == 3 ? headerAuthorizations[2] : "";
-        } else { // headerAuthorization 존재하지 않는다면 쿠키에서 정보가지고 오기
-            apiKey = getCookieValue("apiKey", "");
-            accessToken = getCookieValue("accessToken", "");
-        }
-
-        /*
-            검증
-           1. accessToken, apiKey(refreshToken)가지고 있는지 검증
-        */
-
-        if (apiKey.isBlank()) throw new ServiceException("401-1", "로그인 후 사용해주세요.");
-
-        Member member = null;
-        boolean isAccessTokenExists = !accessToken.isBlank();
-        boolean isAccessTokenValid = false;
-
-        if (isAccessTokenExists) {
-            Map<String, Object> payload = memberService.payload(accessToken);
-
-            if (payload != null) {
-                long id = ((Number) payload.get("id")).longValue();
-                String username = (String) payload.get("username");
-                String nickname = (String) payload.get("nickname");
-                member = new Member(id, username, nickname);
-
-                // 토큰 유효성 검증 성공
-                isAccessTokenValid = true;
-            }
-        }
-
-        if (member == null) {
-            member = memberService.findByApiKey(apiKey)
-                    .orElseThrow(() -> new ServiceException("401-3", "회원을 찾을 수 없습니다."));
-        }
-
-        // 토큰 존재하고, 토큰 유효성 검증 실패 했을 때
-        if (isAccessTokenExists && !isAccessTokenValid) {
-            // apiKey(refresh token)을 이용한 accessToken 재발급
-            String actorAccessToken = memberService.genAccessToken(member);
-
-            setCookie("accessToken", actorAccessToken);
-            // 비교용으로 전달
-            setHeader("Authorization", actorAccessToken);
-        }
-
-        return member;
+        return Optional.ofNullable(
+                        SecurityContextHolder
+                                .getContext()
+                                .getAuthentication()
+                )
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof SecurityUser)
+                .map(principal -> (SecurityUser) principal)
+                .map(securityUser -> new Member(securityUser.getId(), securityUser.getUsername(), securityUser.getNickname()))
+                .orElse(null);
     }
 
     public void setHeader(String name, String value) {
