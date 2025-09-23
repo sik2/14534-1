fc88ce43f1e42894fb90a0d3b3d682b12001e809
sik2 <bickwang123@gmail.com>
2025-09-23 12:35:12 +0900

    103-1

--- src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -34,14 +35,11 @@ public class ApiV1AdmMemberControllerTest {
 
     @Test
     @DisplayName("회원 다건조회")
+    @WithUserDetails("admin")
     void t1() throws Exception {
-        Member actor = memberService.findByUsername("admin").get();
-        String actorApiKey = actor.getApiKey();
-
         ResultActions resultActions = mvc
                 .perform(
                         get("/api/v1/adm/members")
-                            .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
